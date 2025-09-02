de9f8f65be90cdf16f38ecacca87d0b595f545ea
Vitor Nascimento <vitornsc7@gmail.com>
2025-09-02 12:04:34 -0300

    chore: Add API routes for produtos and compras

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\CompraController;
-
+ 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::post('/compras', [CompraController::class, 'store']);
