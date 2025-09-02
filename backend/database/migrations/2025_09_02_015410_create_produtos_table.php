4dabb7c9b85620c55afb341e364228998b244152
Vitor Nascimento <vitornsc7@gmail.com>
2025-09-02 12:00:04 -0300

    fix: Including files that where missing in other commits

--- backend/database/migrations/2025_09_02_015410_create_produtos_table.php
+++ backend/database/migrations/2025_09_02_015410_create_produtos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->decimal('preco_venda', 10, 2);
+            $table->decimal('custo_medio', 10, 2);
+            $table->integer('estoque');
             $table->timestamps();
         });
     }
