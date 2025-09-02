2f2fb317d37aab480f0cbd1ee6f493ed5823e808
Vitor Nascimento <vitornsc7@gmail.com>
2025-09-02 11:57:42 -0300

    feat: Add Compra model with produtos relationship

--- backend/app/Models/Compra.php
+++ backend/app/Models/Compra.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compra extends Model
 {
-    //
+    protected $fillable = ['fornecedor'];
+
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class, 'compra_produto')
+                    ->withPivot('quantidade', 'preco_unitario')
+                    ->withTimestamps();
+    }
 }
