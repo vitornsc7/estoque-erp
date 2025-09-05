5c85529eae430123df5a0d981e7c302100fe86e3
Vitor Nascimento <vitornsc7@gmail.com>
2025-09-05 17:35:15 -0300

    feat: adding base routes and style for components

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,7 +1,34 @@
 <template>
-    <h1>Teste</h1>
+  <div class="container">
+    <ProdutoCadastro @atualizar="atualizarProdutos" />
+    <ProdutoLista ref="listaProdutos" />
+    <CompraRegistro />
+    <VendaRegistro />
+    <VendaCancelar />
+  </div>
 </template>
 
 <script setup>
+import ProdutoCadastro from '../components/ProdutoCadastro.vue'
+import ProdutoLista from '../components/ProdutoLista.vue'
+import CompraRegistro from '../components/CompraRegistro.vue'
+import VendaRegistro from '../components/VendaRegistro.vue'
+import VendaCancelar from '../components/VendaCancelar.vue'
 
+import { ref } from 'vue'
+
+const listaProdutos = ref(null)
+
+const atualizarProdutos = () => {
+  listaProdutos.value.carregarProdutos()
+}
 </script>
+
+<style>
+.container {
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
